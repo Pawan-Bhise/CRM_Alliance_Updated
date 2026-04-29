8862186ae3c17de5e36cec4853b39d736a36a36f
PawanBhise <bhise.pawan@gmail.com>
2026-04-29 23:42:36 +0530

    Initialte survey controller

---
+++ CallCenterSecure/Controllers/SurveyController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace CallCenter.Controllers
+{
+    public class SurveyController : Controller
+    {
+        // GET: Survey
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
